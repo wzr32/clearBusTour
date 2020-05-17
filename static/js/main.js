37ccae5ebfad5bf807048e96d3401633041db5ed
Ruben Padilla <alejandro010492@gmail.com>
2020-05-16 21:30:27 -0400

    sync

--- static/js/main.js
+++ static/js/main.js
@@ -6,4 +6,5 @@ document.querySelector('#btn-menu').addEventListener('click', () => {
 document.querySelector('#btn-networks').addEventListener('click', () => {
     document.querySelector('#m-btn').classList.toggle('active');
     document.querySelector('#w-btn').classList.toggle('active');
+    document.querySelector('#btn-networks').classList.toggle('active');
 });
